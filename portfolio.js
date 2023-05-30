0f26ce336513746edebd82c32be4702d78fa690a
Mayonni <129269004+Mathieu-Maion@users.noreply.github.com>
2023-05-30 15:07:54 +0200

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,28 +1,24 @@
 var translations = {};
+var hideContactBar;
 
 window.addEventListener('DOMContentLoaded', () => {
-  const header = document.getElementsByTagName('header')[0];
-  const a = header.getElementsByTagName('a');
+  headerMenuEvents();
 
-  for(var i = 0; i < a.length ; i++){
-    a[i].addEventListener("click", (event) => {
-      if(!event.target.classList.contains("selected")){
-        const selectedElement = header.getElementsByClassName("selected")[0];
-        selectedElement.classList.remove("selected");
+  
+  if(window.innerWidth <= 900) hideContactBar = true;
+  else hideContactBar = false;
+  if (hideContactBar) showContactBar(true);
 
-        const targetElementClass = event.target.classList[0];
-        const selectedElementClass = selectedElement.classList[0];
-
-        event.target.classList.add("selected");
-
-        const contentToShow = document.getElementsByTagName("main")[0].getElementsByClassName(targetElementClass)[0];
-        const contentToHide = document.getElementsByTagName("main")[0].getElementsByClassName(selectedElementClass)[0];
-        contentToHide.setAttribute("hidden", true);
-        contentToShow.removeAttribute("hidden");
-      }
-    });
-  }
-
+  addEventListener("resize", (event) => {
+    const width = window.innerWidth;
+    if(width <= 900 && hideContactBar){
+      showContactBar(true);
+      hideContactBar = false;
+    } else if(width > 900 && !hideContactBar){
+      showContactBar(true);
+      hideContactBar = true;
+    }
+  });
 });
 
 
@@ -62,3 +58,62 @@ function showCV(){
   else window.open('ressources/cv_en.pdf');
 }
 
+function headerMenuEvents(){
+  const header = document.getElementsByTagName('header')[0];
+  const a = header.getElementsByTagName('a');
+
+  for(var i = 0; i < a.length ; i++){
+    a[i].addEventListener("click", (event) => {
+      if(!event.target.classList.contains("selected")){
+        const selectedElement = header.getElementsByClassName("selected")[0];
+        selectedElement.classList.remove("selected");
+
+        const targetElementClass = event.target.classList[0];
+        const selectedElementClass = selectedElement.classList[0];
+
+        event.target.classList.add("selected");
+
+        const contentToShow = document.getElementsByTagName("main")[0].getElementsByClassName(targetElementClass)[0];
+        const contentToHide = document.getElementsByTagName("main")[0].getElementsByClassName(selectedElementClass)[0];
+        contentToHide.setAttribute("hidden", true);
+        contentToShow.removeAttribute("hidden");
+      }
+    });
+  }
+}
+
+function showContactBar(event = false){
+  const header = document.getElementsByTagName('header')[0];
+  const main = document.getElementsByTagName('main')[0];
+  const button = document.getElementById("ShowContactBarButton");
+  if(event){
+    slideContactBar(hideContactBar);
+    if(!hideContactBar){
+      button.style.right = "5px";
+    } else {
+      button.style.right = "-40px";
+      header.style.marginRight = "310px";
+      main.style.marginRight = "310px";
+    } 
+  } else {
+      slideContactBar(hideContactBar);
+  }
+}
+
+function slideContactBar(slide){
+  const contactBar = document.getElementsByClassName("contact-bar")[0];
+  const header = document.getElementsByTagName('header')[0];
+  const main = document.getElementsByTagName('main')[0];
+  const button = document.getElementById("ShowContactBarButton");
+  if(slide){
+    contactBar.classList.add("slide");
+    header.style.marginRight = "0px";
+    main.style.marginRight = "0px";
+    button.style.backgroundImage = 'url("./images/arrowLeft.png")';
+    hideContactBar = false;
+  } else {
+    contactBar.classList.remove("slide");
+    button.style.backgroundImage = 'url("./images/arrowRight.png")';
+    hideContactBar = true;
+  }
+}
